c8bd86ab1255633dfd4e42c3023b728eac03ac1e
RT <rt@192.168.0.103>
2014-05-09 20:08:03 +0200

    before RMI

---
+++ src/bank/http/HttpServlet.java
@@ -0,0 +1,10 @@
+package bank.http;
+
+public class HttpServlet extends javax.servlet.http.HttpServlet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
