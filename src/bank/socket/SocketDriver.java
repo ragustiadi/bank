e523a5520aa00784334a1d7ade137d59d0ae9948
RT <rt@192.168.0.103>
2014-05-22 11:42:24 +0200

    Socket solution hinzugefügt

---
+++ src/bank/socket/SocketDriver.java
@@ -0,0 +1,48 @@
+package bank.socket;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import bank.Bank;
+
+/*
+ * Klasse SocketDriver ist verantwortlich für die Kommunikation auf der Seite
+ * des Clients.
+ */
+public class SocketDriver implements bank.BankDriver {
+
+	bank.Bank bank = null;
+	Socket connection = null;
+	ObjectInputStream dataIn = null;
+	ObjectOutputStream dataOut = null;
+
+	public void connect(String[] args) throws IOException {
+		if (args.length < 2) {
+			System.out.println("No host IP-Address and port provided. Fallback to localhost:6789");
+			connection = new Socket("localhost", 6789);
+		} else {
+			connection = new Socket(args[0], Integer.parseInt(args[1]));
+		}
+		System.out.println("Connection to " + connection.getInetAddress()
+				+ " established on port " + connection.getLocalPort());
+		dataIn = new ObjectInputStream(connection.getInputStream());
+		dataOut = new ObjectOutputStream(connection.getOutputStream());
+		bank = new SocketBank(this);
+	}
+
+	public void disconnect() throws IOException {
+		dataOut.writeObject(null);
+		dataIn.close();
+		dataOut.close();
+		connection.close();
+		System.out.println("Connection to " + connection.getInetAddress()
+				+ " closed.");
+	}
+
+	public Bank getBank() {
+		return bank;
+	}
+
+}
---
+++ src/bank/socket/SocketServer.java
@@ -0,0 +1,80 @@
+package bank.socket;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+ * Klasse Server startet auf der Serverseite und startet für jeden neuen Client
+ * einen neuen Thread.
+ */
+public class SocketServer implements Runnable {
+
+	static ServerSocket socket = null;
+	Socket connection = null;
+	static ServerBank bank = new ServerBank();
+	ObjectInputStream dataIn = null;
+	ObjectOutputStream dataOut = null;
+	static int numOfConnections = 0;
+
+	public SocketServer(Socket s) {
+		connection = s;
+		try {
+			dataOut = new ObjectOutputStream(connection.getOutputStream());
+			dataIn = new ObjectInputStream(connection.getInputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		int port = 6789; // default port number
+		if (args.length > 0)
+			port = Integer.parseInt(args[0]); // overwrite default port if args are set
+		try {
+			socket = new ServerSocket(port);
+			System.out.println("Server ready. Accepting connections on port "
+					+ port);
+			while (true) {
+				Socket connection = socket.accept();
+				Thread t = new Thread(new SocketServer(connection));
+				t.start();
+				numOfConnections++;
+				System.out.println("Number of clients: " + numOfConnections);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		System.out.println("Thread for " + connection.getInetAddress()
+				+ " started");
+		bank.commands.Command cmd = null;
+		while (!connection.isClosed()) {
+			try {
+				cmd = (bank.commands.Command) dataIn.readObject();
+				if (cmd == null)
+					connection.close();
+				else
+					cmd.execute(bank, dataOut);
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			dataIn.close();
+			dataOut.close();
+			connection.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Thread for " + connection.getInetAddress()
+				+ " terminated");
+		numOfConnections--;
+	}
+}
